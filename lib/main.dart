453ffdeff5fbcdd4cfd506ec7f44ad72f2e9e4b7
buddy5230 <74503111+buddy5230@users.noreply.github.com>
2023-01-20 19:42:09 +0700

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -8,44 +8,77 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final List<String> items;
 
   const MyApp({super.key, required this.items});
 
   @override
-  Widget build(BuildContext context) {
-    const title = 'Pokemon List';
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  int _favoriteCount = 0;
+
+  void _incrementFavoriteCount() {
+    setState(() {
+      _favoriteCount++;
+    });
+  }
+
+  void _decrementFavoriteCount() {
+    setState(() {
+      _favoriteCount--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: title,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(title),
+          title: Center(
+            child: Text('Pokemon List ($_favoriteCount)')
+          ),
         ),
         body: ListView.builder(
-          itemCount: items.length ~/ 3, // 3 items per row so we divide the number of items by 3
+          itemCount: widget.items.length ~/ 3,
           itemBuilder: (context, index) {
             return Row(
               children: [
-                Expanded(child: Stack( // Stack is for overlaying Favorite icon on top of image
-                  children: [
-                    Image.network(items[index*3]),
-                    const Icon(Icons.favorite_border, size: 50,),
-                  ],
-                )),
-                Expanded(child: Stack(
-                  children: [
-                    Image.network(items[index*3+1]),
-                    const Icon(Icons.favorite_border, size: 50,),
-                  ],
-                )),
-                Expanded(child: Stack(
-                  children: [
-                    Image.network(items[index*3+2]),
-                    const Icon(Icons.favorite_border, size: 50,),
-                  ],
-                )),
+                Expanded(
+                  child: Stack(
+                    children: [
+                      Image.network(widget.items[index * 3]),
+                      FavoriteWidget(
+                        onIncrement: _incrementFavoriteCount,
+                        onDecrement: _decrementFavoriteCount,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Stack(
+                    children: [
+                      Image.network(widget.items[index * 3 + 1]),
+                      FavoriteWidget(
+                        onIncrement: _incrementFavoriteCount,
+                        onDecrement: _decrementFavoriteCount,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Stack(
+                    children: [
+                      Image.network(widget.items[index * 3 + 2]),
+                      FavoriteWidget(
+                        onIncrement: _incrementFavoriteCount,
+                        onDecrement: _decrementFavoriteCount,
+                      ),
+                    ],
+                  ),
+                ),
               ],
             );
           },
@@ -54,3 +87,42 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class FavoriteWidget extends StatefulWidget {
+  final void Function() onIncrement;
+  final void Function() onDecrement;
+
+  const FavoriteWidget({super.key, required this.onIncrement, required this.onDecrement});
+
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = false;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        widget.onDecrement();
+        _isFavorited = false;
+      } else {
+        widget.onIncrement();
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: _toggleFavorite,
+      icon: (_isFavorited
+          ? const Icon(Icons.favorite, size: 50,)
+          : const Icon(Icons.favorite_border, size: 50,)),
+      color: Colors.red,
+    );
+  }
+}
+
+
